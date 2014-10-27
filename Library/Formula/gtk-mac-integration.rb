f45c943a71b60aaba4018ca5aaf79b80b99fe1e8
Gui13 <guitreize@gmail.com>
2014-10-27 14:47:21 +0100

    Update gtk-mac-integration.rb
    The 2.0.1 has a typo in a text which prevents the compilation. This version has not (and it's the last version also).

--- Library/Formula/gtk-mac-integration.rb
+++ Library/Formula/gtk-mac-integration.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GtkMacIntegration < Formula
   homepage 'https://live.gnome.org/GTK+/OSX'
-  url 'ftp://ftp.gnome.org/pub/gnome/sources/gtk-mac-integration/2.0/gtk-mac-integration-2.0.1.tar.xz'
-  sha1 '9d939a2e4fb6c0ab4fe3d544ac712a152451249a'
+  url 'ftp://ftp.gnome.org/pub/gnome/sources/gtk-mac-integration/2.0/gtk-mac-integration-2.0.5.tar.xz'
+  sha1 '74e43c2fff14ca4aff3a8f6c35660bedc7a349a1'
   head 'git://git.gnome.org/gtk-mac-integration'
 
   depends_on 'pkg-config' => :build
